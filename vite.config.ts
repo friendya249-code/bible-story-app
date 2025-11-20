cc31f8f69088c668142ad152cfe3020f2a29bbce
friendya249-code <friendya249@gmail.com>
2025-11-20 21:51:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
     plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY) 
+    },
+    server: {
+      host: true
+    }
+  }
+})import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY) 
+    },
+    server: {
+      host: true
+    }
+  }
 })
