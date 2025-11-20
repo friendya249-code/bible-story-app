e9dcebfadd2f9f7f243ece679b3dc649e0a0e01b
friendya249-code <friendya249@gmail.com>
2025-11-20 21:58:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,19 +13,4 @@ export default defineConfig(({ mode }) => {
       host: true
     }
   }
-})import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY) 
-    },
-    server: {
-      host: true
-    }
-  }
 })
